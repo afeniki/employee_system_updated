b0778020bf16f0ab3b566feb969fc48330c3b69a
AYOOLA SINMILOLUWA <akinbayo.oluwanifemi@lmu.edu.ng>
2020-06-24 14:18:28 +0100

    added image placeholder

--- db_main.py
+++ db_main.py
@@ -8,6 +8,24 @@ import os
 import shutil
 import db_config
 
+def on_tab_selected(event):
+    selected_tab = event.widget.select()
+    tab_text = event.widget.tab(selected_tab, "text")
+
+    if tab_text == "All Records":
+        print("All records tab selected")
+    if tab_text == "Add New Record":
+        print("Add new records tab selected")
+
+def load_database_results():
+    return
+
+
+file_name = "default.png"
+path = db_config.PHOTO_DIRECTORY + file_name
+rows = None #none is an object that contains nothing
+num_of_rows = None
+
 form = tk.Tk()
 form.title("Database Form")
 form.geometry("500x280")
@@ -15,6 +33,8 @@ form.geometry("500x280")
 tab_parent = ttk.Notebook(form)  #helps to set up(design and manage) tabs
 tab1 = ttk.Frame(tab_parent)
 tab2 = ttk.Frame(tab_parent)
+
+tab_parent.bind("<<NotebookTabChanged>>", on_tab_selected)
 
 tab_parent.add(tab1, text="All Records")
 tab_parent.add(tab2, text="Add New Record")
@@ -28,7 +48,9 @@ firstEntryTabOne = tk.Entry(tab1)
 familyEntryTabOne = tk.Entry(tab1)
 jobEntryTabOne = tk.Entry(tab1)
 
-imgLabelTabOne = tk.Label(tab1)
+openImageTabOne = Image.open(path)
+imgTabOne = ImageTk.PhotoImage(openImageTabOne)
+imgLabelTabOne = tk.Label(tab1, image=imgTabOne)
 
 buttonForward = tk.Button(tab1, text="Forward")
 buttonBack = tk.Button(tab1, text="Back")
@@ -45,6 +67,10 @@ jobEntryTabOne.grid(row=2, column=1, padx=15, pady=15)
 
 imgLabelTabOne.grid(row=0, column=2, rowspan=3, padx=15, pady=15)
 
+buttonBack.grid(row=3, column=0, padx=15, pady=15)
+buttonForward.grid(row=3, column=2, padx=15, pady=15)
+
+
 #ADDING WIDGETS FOR TAB2
 firstLabelTabTwo = tk.Label(tab2, text="FirstName:")
 familyLabelTabTwo = tk.Label(tab2, text="FamilyName:")
@@ -54,7 +80,9 @@ firstEntryTabTwo = tk.Entry(tab2)
 familyEntryTabTwo = tk.Entry(tab2)
 jobEntryTabTwo = tk.Entry(tab2)
 
-imgLabelTabTwo = tk.Label(tab2)
+openImageTwo = Image.open(path)
+imageTabTwo = ImageTk.PhotoImage(openImageTabOne)
+imgLabelTabTwo = tk.Label(tab2, image=imageTabTwo)
 
 buttonCommit = tk.Button(tab2, text="Add Record To Database")
 buttonAddImage = tk.Button(tab2, text="Add Image")
@@ -71,10 +99,16 @@ jobEntryTabTwo.grid(row=2, column=1, padx=15, pady=15)
 
 imgLabelTabTwo.grid(row=0, column=2, rowspan=3, padx=15, pady=15)
 
+
 buttonCommit.grid(row=4, column=1, padx=15, pady=15)
 buttonAddImage.grid(row=4, column=2, padx=15, pady=15)
 
 
 tab_parent.pack(expand=1, fill="both")
 
+# Set up a connection object
+# Write an SQL
+# Set cursor object
+# Fetch rows back from the database table
+# Display result
 form.mainloop()
